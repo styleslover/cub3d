6d341b285420108332613aa3e169cb5ec4ffc774
Mariel Abrigo <mabrigo@c2r2p12.42firenze.it>
2025-02-21 11:57:16 +0100

    parsing funzionante

--- parsing.c
+++ parsing.c
@@ -6,7 +6,7 @@
 /*   By: mabrigo <mabrigo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:47:17 by mabrigo           #+#    #+#             */
-/*   Updated: 2025/02/19 21:32:59 by mabrigo          ###   ########.fr       */
+/*   Updated: 2025/02/21 11:54:51 by mabrigo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*strcmp_from_i(int i, char *src)
 	if (len <= 0)
 		return (NULL);
 	dest = malloc(sizeof(char) * len);
-	if (!dest) 
+	if (!dest)
 		return (NULL);
 	j = 0;
 	while (src[i])
@@ -54,41 +54,48 @@ void	parse_config_line(char *str, t_map *map)
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
 	if (ft_strncmp(str, "NO ", 3) == 0)
-		map->north_txtr = strcmp_from_i(i, str);
+		map->north_txtr = strcmp_from_i(i + 3, str);
 	else if (ft_strncmp(str, "SO ", 3) == 0)
-		map->south_txtr = strcmp_from_i(i, str);
+		map->south_txtr = strcmp_from_i(i + 3, str);
 	else if (ft_strncmp(str, "WE ", 3) == 0)
-		map->west_txtr = strcmp_from_i(i, str);
+		map->west_txtr = strcmp_from_i(i + 3, str);
 	else if (ft_strncmp(str, "EA ", 3) == 0)
-		map->east_txtr = strcmp_from_i(i, str);
+		map->east_txtr = strcmp_from_i(i + 3, str);
 	else if (ft_strncmp(str, "F ", 2) == 0)
-		map->floor_color = strcmp_from_i(i, str);
+		map->floor_color = strcmp_from_i(i + 2, str);
 	else if (ft_strncmp(str, "C ", 2) == 0)
-		map->ceiling_color = strcmp_from_i(i, str);
+		map->ceiling_color = strcmp_from_i(i + 2, str);
 }
 
 int	is_map_line(char *str)
 {
-	if (!*str)
+	int	i;
+
+	i = 0;
+	if (!str[i])
 		return (0);
-	while (*str && *str == ' ')
-		str++;
-	if (*str != '1' && *str != '0' && *str != 'N'
-		&& *str != 'S' && *str != 'E' && *str != 'W')
-		return (0);
-	return (1);
+	while (str[i] && str[i] == ' ')
+		i++;
+	if (str[i] == '1' || str[i] == '0' || str[i] == 'N'
+		|| str[i] == 'S' || str[i] == 'E' || str[i] == 'W')
+	{
+		if (str[i + 1] == 'O' || str[i + 1] == 'A' || str[i + 1] == 'E')
+			return (0);
+		else
+			return (1);
+	}
+	return (0);
 }
+
 
 void	parse_file(int fd, t_map *map)
 {
 	char	*line;
 	int		line_len;
 
-	line = get_next_line(fd);
-	map->map_width = 0;
-	map->map_height = 0;
+	init_map(map);
 	line_len = 0;
-	while (line)
+	while ((line = get_next_line(fd)))
 	{
 		if (is_map_line(line) != 0)
 		{
@@ -100,10 +107,9 @@ void	parse_file(int fd, t_map *map)
 		else
 			parse_config_line(line, map);
 		free(line);
-		line = get_next_line(fd);
 	}
-	map->map_width *= 64;
-	map->map_height *= 64;
+	map->map_width *= TILE_SIZE;
+	map->map_height *= TILE_SIZE;
 	printf("North Texture: %s\n", map->north_txtr);
 	printf("South Texture: %s\n", map->south_txtr);
 	printf("West Texture: %s\n", map->west_txtr);
